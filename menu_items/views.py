c7cf76d8df1b0e2b5c581132a88434ebdd091de9
MariuszKaminski <mkaminski10@gmail.com>
2022-06-06 23:02:50 +0000

    Added product views and templates

--- menu_items/views.py
+++ menu_items/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Item
 
 # Create your views here.
+
+def all_menu_items(request):
+    """ A view to show all menu items, including sorting and search queries """
+
+    menu_items = Item.objects.all()
+
+    context = {
+        'menu_items': menu_items,
+    }
+    
+    return render(request, 'menu_items/menu_items.html', context)
