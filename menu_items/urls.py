cb463ace197dcf7d361796fc90a3afc8c230d820
MariuszKaminski <mkaminski10@gmail.com>
2022-06-09 23:40:13 +0000

    added item detail veiw and template

--- menu_items/urls.py
+++ menu_items/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.all_menu_items, name='menu_items')
+    path('', views.all_menu_items, name='menu_items'),
+    path('<item_id>', views.item_detail, name='item_detail'),
 ]
--- menu_items/views.py
+++ menu_items/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Item
 
 # Create your views here.
 
 def all_menu_items(request):
-    """ A view to show all menu items, including sorting and search queries """
+    """ A view to show individual product details """
 
     menu_items = Item.objects.all()
 
@@ -13,3 +13,19 @@ def all_menu_items(request):
     }
     
     return render(request, 'menu_items/menu_items.html', context)
+
+from django.shortcuts import render
+from .models import Item
+
+# Create your views here.
+
+def item_detail(request, item_id):
+    """ A view to show all menu items, including sorting and search queries """
+
+    menu_item = get_object_or_404(Item, ik=item_id)
+
+    context = {
+        'menu_item': menu_item,
+    }
+    
+    return render(request, 'menu_items/item_detail.html', context)
