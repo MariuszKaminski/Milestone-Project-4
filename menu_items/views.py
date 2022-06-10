b4e50fa715e9e34e64998e63e82b7f9b0a821765
MariuszKaminski <mkaminski10@gmail.com>
2022-06-10 19:47:37 +0000

    Added search functionality

--- menu_items/views.py
+++ menu_items/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Item
 
 # Create your views here.
@@ -7,9 +9,21 @@ def all_menu_items(request):
     """ A view to show individual product details """
 
     menu_items = Item.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search critieria!")
+                return redirect(reverse('menu_items'))
+            
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            menu_items = menu_items.filter(queries)
 
     context = {
         'menu_items': menu_items,
+        'search_term': query,
     }
     
     return render(request, 'menu_items/menu_items.html', context)
