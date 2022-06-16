3038cbe210ff1487cc53cdf16289ba3ef2988a70
MariuszKaminski <mkaminski10@gmail.com>
2022-06-16 01:00:21 +0000

    Added checkout views and payments

--- protein_shakes_n_snacks/settings.py
+++ protein_shakes_n_snacks/settings.py
@@ -46,6 +46,9 @@ INSTALLED_APPS = [
     'menu_items',
     'bag',
     'checkout',
+
+    # Other
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -59,6 +62,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'protein_shakes_n_snacks.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -74,8 +79,13 @@ TEMPLATES = [
                 'django.template.context_processors.request', # required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'bag.contexts.bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
